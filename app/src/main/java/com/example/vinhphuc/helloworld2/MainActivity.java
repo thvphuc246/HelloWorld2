e7253d99c657b580e6a44de25908b6813d2496e2
Phuc Truong <truonghoangvinhphuc@gmail.com>
2017-05-09 15:18:29 +0300

    Upload project files

---
+++ app/src/main/java/com/example/vinhphuc/helloworld2/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.vinhphuc.helloworld2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    protected void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+        setContentView(R.layout.activity_main);
+    }
+}
